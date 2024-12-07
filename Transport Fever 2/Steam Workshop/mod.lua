4ded5c8e9dcfa7e71a075913e4a1d46c77592f75
Aliaksei Kapura <aliaksei.kapura@any-ware.nl>
2024-12-07 15:28:22 +0300

    Абнаўленьне інфармацыі моду

--- Transport Fever 2/Steam Workshop/mod.lua
+++ Transport Fever 2/Steam Workshop/mod.lua
@@ -5,7 +5,7 @@ function data()
             severityAdd = "NONE",
             severityRemove = "NONE",
             name = _("Беларускі пераклад"),
-            description = "Аматарскі пераклад на беларускую мову (клясічная).\nДля ўжываньня ў наладках гульні беларускую мову.\nМагчымы памылкі.",
+            description = "Аматарскі пераклад на беларускую (клясічны).\n\nКірыліца:\n- Каб ужыць пераклад, перайдзіце да налад гульні й выберыце ў мовах \"Беларуская (Mod)\"\n- Каб ужыць беларускія назвы мест/вуліц/людзей, пад час стварэньня гульні або запуску існуючай уключыце мод. \"Беларускі пераклад\", далей у Пашыраных наладах для \"Назвы мест\" выберыце \"Беларускі пераклад\"\n\nŁacinka [ŭ raspracoŭcy]:\n- Kab užyć pierakład, pierajdzicie da naład hulni j vybierycie ŭ movach \"Biełaruskaja (Mod)\"",
             tags = { "Localization", "Belarusian" },
             authors = {
                 {
@@ -15,7 +15,7 @@ function data()
             },
         },
         options = {
-            nameList = { { "belarus", _("Беларускі") } },
+            nameList = { { "belarus", _("Беларускі пераклад") } },
         },
         runFn = function(settings)
         end
